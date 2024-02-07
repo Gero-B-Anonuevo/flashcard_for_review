67d23e0596bc974c0faab886bd8a80ea6fe90d1d
Anonuevo <geroanonuevo3@gmail.com>
2024-02-07 21:28:43 +0800

    still not working

--- prog.py
+++ prog.py
@@ -12,8 +12,16 @@ if __name__ == '__main__':
     main_window.geometry('500x400')
 
     #APPLY STYLING TO GUI
-    decor = style(theme='superhero')
-    decor.configure('TLabel', font= ('TkDefaultFont', 18))
-    decor.configure('TButton', font= ('TkDefaultFont', 16))
+    style = Style(theme='superhero')
+    style.configure('TLabel', font= ('TkDefaultFont', 18))
+    style.configure('TButton', font= ('TkDefaultFont', 16))
+
+    #STORING USER INPUTS ON VARIABLES
+    set_name_var = tk.StringVar()
+    word_var = tk.StringVar()
+    definition_var = tk.StringVar()
+
+    #NOTEBOOK WIDGET FOR MANAGING TABS
+    notebook = ttk.Notebook(main_window)
 
     main_window.mainloop()
